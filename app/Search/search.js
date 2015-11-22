1c5234af6c70f65c8eac0a3cddd16e1be8cfc2da
jsivakumaran <jsivakumaran@outlook.com>
2015-11-21 17:15:22 -0800

    Add bootstrap styling to search results

--- app/Search/search.js
+++ app/Search/search.js
@@ -11,7 +11,18 @@ angular.module('myApp.search', ['ngRoute'])
 
 .controller('Search2Ctrl', ['$scope', '$filter', function($scope, $filter) {
 
-  $scope.activities = "xyz";
+  $scope.activities = [
+    {name:'John', age:25, gender:'boy', activity:'hiking'},
+    {name:'Jessie', age:30, gender:'girl', activity:'hiking'},
+    {name:'Johanna', age:28, gender:'girl', activity:'hiking'},
+    {name:'Joy', age:15, gender:'girl', activity:'hiking'},
+    {name:'Mary', age:28, gender:'girl', activity:'running'},
+    {name:'Peter', age:95, gender:'boy', activity:'running'},
+    {name:'Sebastian', age:50, gender:'boy', activity:'running'},
+    {name:'Erika', age:27, gender:'girl', activity:'walking'},
+    {name:'Patrick', age:40, gender:'boy', activity:'walking'},
+    {name:'Samantha', age:60, gender:'girl', activity:'walking'}
+  ];
 
   /* $scope.runsearch = function(){
     console.log("Connected")
